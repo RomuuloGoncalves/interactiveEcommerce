a76bc1bc24cd71d135d2670d822ebfb460fe33a8
Rômulo Gonçalves <romulogoncalves13@gmail.com>
2022-09-30 15:26:52 -0300

    #

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 const imagens = document.querySelectorAll(".imagens__escolha");
+const modal = document.querySelector(".modal__carrinho")
+const quantidade = document.getElementById("quantidade");
 
 let storangeFunctions = {
-	guardar: (valor) =>
-		localStorage.setItem("quantidade", JSON.stringify(valor)) || "",
+	guardar: (valor) =>localStorage.setItem("quantidade", JSON.stringify(valor)) || "",
 	pegar: () => JSON.parse(localStorage.getItem("quantidade")) || "0",
 };
 
 function incrementar(valor) {
-	let quantidade = document.getElementById("quantidade");
-	quantidade.innerHTML = Number(quantidade.innerHTML) + valor;
-	storangeFunctions.guardar(quantidade.innerHTML);
+	let numero = Number(quantidade.innerHTML) + valor;
+	
+	if(numero >= 0){
+		quantidade.innerHTML = numero
+		storangeFunctions.guardar(quantidade.innerHTML);
+	}
 }
 
 function mudarImagem(imagemClicada, posicao) {
@@ -26,8 +30,37 @@ function mudarImagem(imagemClicada, posicao) {
 }
 
 function modalCarrinho(){
-	modal = document.querySelector(".modal__carrinho")
 	modal.classList.toggle ("active")
+}
+
+function adicionarCarrinho(){
+	modal.innerHTML = ""
+	let html = `
+	<p id="cart">Cart</p>
+	<div class="compra">
+		<img src="./img/image-product-1-thumbnail.jpg"
+			onclick="mudarImagem('./img/image-product-1.jpg', 0)" alt="">
+		<div class="info">
+			<p>Fall Limited Edition Seneakers</p>
+			<p>$125.00 X ${quantidade.innerHTML} <span>$ ${125.00 * Number(quantidade.innerHTML)}</span></p>
+		</div>
+		<div class="lixo" onclick="limparCarrinho()"><img src="./img/icon-delete.svg" alt=""></div>
+	</div>
+	`
+	modal.innerHTML += html
+
+	modalCarrinho()
+
+	setTimeout(modalCarrinho, 1000)
+}
+
+function limparCarrinho(){
+	modal.innerHTML = ""
+	let html = `
+	<p id="cart">Cart</p>
+	`
+	modal.innerHTML += html
+	modalCarrinho()
 }
 
 document.getElementById("quantidade").innerHTML = storangeFunctions.pegar();
