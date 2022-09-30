b1b666cff64e03ee0165b354870f61c8359b753d
Rômulo Gonçalves <romulogoncalves13@gmail.com>
2022-09-30 14:26:38 -0300

    #

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
+const imagens = document.querySelectorAll(".imagens__escolha")
+
+let storangeFunctions = {
+        guardar: (valor)=> localStorage.setItem("quantidade", JSON.stringify(valor)) || "",
+        pegar: ()=> JSON.parse(localStorage.getItem("quantidade")) || "0",
+}
+
 function incrementar(valor){
         let quantidade = document.getElementById("quantidade")
         quantidade.innerHTML = Number(quantidade.innerHTML) + valor
         storangeFunctions.guardar(quantidade.innerHTML)
 }
 
-let storangeFunctions = {
-        guardar: (valor)=> localStorage.setItem("quantidade", JSON.stringify(valor)) || "",
-        pegar: ()=> JSON.parse(localStorage.getItem("quantidade")) || "0",
+function mudarImagem(imagemClicada, posicao){
+        let imagemPrincipal = document.getElementById("imagemPrincipal")
+        let imagem = imagens[posicao]
+        imagens.forEach((elemento)=>{
+                elemento.style.border = "none"
+        })
+
+        imagemPrincipal.src = imagemClicada
+        imagem.style.border = "2px solid orange"
 }
 
 document.getElementById("quantidade").innerHTML = storangeFunctions.pegar()
