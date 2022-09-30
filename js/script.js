929e3f83b7cf87801209a672b93811320f2cccaa
Rômulo Gonçalves <romulogoncalves13@gmail.com>
2022-09-30 14:32:38 -0300

    #

--- js/script.js
+++ js/script.js
@@ -1,25 +1,28 @@
-const imagens = document.querySelectorAll(".imagens__escolha")
+const imagens = document.querySelectorAll(".imagens__escolha");
 
 let storangeFunctions = {
-        guardar: (valor)=> localStorage.setItem("quantidade", JSON.stringify(valor)) || "",
-        pegar: ()=> JSON.parse(localStorage.getItem("quantidade")) || "0",
+	guardar: (valor) =>
+		localStorage.setItem("quantidade", JSON.stringify(valor)) || "",
+	pegar: () => JSON.parse(localStorage.getItem("quantidade")) || "0",
+};
+
+function incrementar(valor) {
+	let quantidade = document.getElementById("quantidade");
+	quantidade.innerHTML = Number(quantidade.innerHTML) + valor;
+	storangeFunctions.guardar(quantidade.innerHTML);
 }
 
-function incrementar(valor){
-        let quantidade = document.getElementById("quantidade")
-        quantidade.innerHTML = Number(quantidade.innerHTML) + valor
-        storangeFunctions.guardar(quantidade.innerHTML)
+function mudarImagem(imagemClicada, posicao) {
+	let imagemPrincipal = document.querySelectorAll("#imagem__principal img");
+	let imagem = imagens[posicao];
+	imagens.forEach((elemento) => {
+		elemento.style.border = "none";
+		elemento.style.opacity = "1";
+	});
+
+	imagemPrincipal.src = imagemClicada;
+	imagem.style.border = "2px solid orange";
+	imagem.style.opacity = "0.5";
 }
 
-function mudarImagem(imagemClicada, posicao){
-        let imagemPrincipal = document.getElementById("imagemPrincipal")
-        let imagem = imagens[posicao]
-        imagens.forEach((elemento)=>{
-                elemento.style.border = "none"
-        })
-
-        imagemPrincipal.src = imagemClicada
-        imagem.style.border = "2px solid orange"
-}
-
-document.getElementById("quantidade").innerHTML = storangeFunctions.pegar()
+document.getElementById("quantidade").innerHTML = storangeFunctions.pegar();
